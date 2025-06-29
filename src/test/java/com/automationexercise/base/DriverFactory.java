719e6f8aee2a818fdd53e047dcc140fe36c63492
Vijeth Ganapatigouda Patil <vpatil29@asu.edu>
2025-06-29 15:36:19 -0700

    FIX: Configure Chrome for headless CI/CD execution

--- src/test/java/com/automationexercise/base/DriverFactory.java
+++ src/test/java/com/automationexercise/base/DriverFactory.java
@@ -2,29 +2,37 @@ package com.automationexercise.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions; // <-- IMPORT ADDED
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverFactory {
 
     public static WebDriver getDriver(String browser) {
-        WebDriver driver;
         switch (browser.toLowerCase()) {
             case "chrome":
-                // Selenium Manager will automatically download the correct driver
-                driver = new ChromeDriver();
-                break;
+                // DEFINITIVE FIX for CI/CD environment
+                ChromeOptions options = new ChromeOptions();
+                // Run Chrome in headless mode (no UI)
+                options.addArguments("--headless");
+                // The next two arguments are required for running in a Linux/Docker container like GitHub Actions
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                // Set a custom window size, which is important for headless mode
+                options.addArguments("--window-size=1920,1080");
+                
+                return new ChromeDriver(options);
+
             case "firefox":
-                // Selenium Manager will automatically download the correct driver
-                driver = new FirefoxDriver();
-                break;
+                // Similar options can be added for Firefox if needed
+                return new FirefoxDriver();
+                
             case "edge":
-                // Selenium Manager will automatically download the correct driver
-                driver = new EdgeDriver();
-                break;
+                // Similar options can be added for Edge if needed
+                return new EdgeDriver();
+                
             default:
                 throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
-        return driver;
     }
 }
